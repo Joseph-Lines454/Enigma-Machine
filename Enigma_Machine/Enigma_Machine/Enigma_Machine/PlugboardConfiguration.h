7f55203e51bbfc8bf1c681ec4648a6d143c11ff7
Joseph Lines <joelines194@gmail.com>
2026-09-24 13:43:15 +0100

    Starting to create the plugboard for the engima machine

--- Enigma_Machine/Enigma_Machine/Enigma_Machine/PlugboardConfiguration.h
+++ Enigma_Machine/Enigma_Machine/Enigma_Machine/PlugboardConfiguration.h
@@ -19,6 +19,79 @@ private:
 
 	char plugboardSettings[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	char plugboardSettingsOrigonal[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+
+
+	
+	float InitializeCircles(std::vector<sf::CircleShape>& vector, sf::Vector2f startPosition)
+	{
+
+		for (int i = 0; i <= 25; i++)
+		{
+			vector.push_back(sf::CircleShape());
+		}
+
+		for (int i = 0; i <= 25; i++)
+		{
+			vector[i].setRadius(27.f);
+			vector[i].setPosition(startPosition);
+			vector[i].setFillColor(sf::Color(40, 40, 40));
+			vector[i].setOutlineThickness(2.f);
+			vector[i].setOutlineColor(sf::Color(180, 180, 180));
+			startPosition.x = startPosition.x + vector[i].getRadius() * 3;
+
+			if (i == 8)
+			{
+
+				startPosition = { (vector[i].getRadius() * 2) + 28.f, startPosition.y + vector[i].getRadius() * 3 };
+
+
+			}
+			else if (i == 16)
+			{
+				startPosition = { 40.f, startPosition.y + vector[i].getRadius() * 3 };
+			}
+		}
+
+		return vector[0].getRadius();
+
+	}
+
+	void InitializeText(std::vector<sf::Text>& text, sf::Vector2f startPosition, sf::Font& font, float circleRadius)
+	{
+		for (int i = 0; i <= 25; i++)
+		{
+
+			text.push_back(sf::Text(font));
+		}
+		for (int i = 0; i <= 25; i++)
+		{
+
+			text[i].setFillColor(sf::Color{ 230,255,215 });
+			//casting to float 
+			text[i].setPosition(startPosition);
+			startPosition.x = startPosition.x + circleRadius * 3.f;
+
+
+			if (i == 8)
+			{
+
+				startPosition = { (circleRadius * 2.95f) + 20.f, startPosition.y + circleRadius * 3 };
+
+
+			}
+			else if (i == 16)
+			{
+				startPosition = { (circleRadius + 35.f), startPosition.y + circleRadius * 3 };
+			}
+
+			text[i].setString(char(i + 65));
+
+
+		}
+	}
+
+
+
 public:
 
 	int PlugboardSettings()
@@ -106,15 +179,25 @@ public:
 		}
 
 	}
-	void RenderValues(sf::RenderWindow& window, sf::Text& textDisplayTitle, sf::Sprite& background)
+	void RenderValues(sf::RenderWindow& window, sf::Text& textDisplayTitle, sf::Sprite& background,std::vector<sf::CircleShape>& vector, std::vector<sf::Text>& textDisplay)
 	{
 		window.draw(background);
 		window.draw(textDisplayTitle);
+		for (int i = 0; i < 26; i++)
+		{
+			window.draw(vector[i]);
+		}
+		for (int i = 0; i < 26; i++)
+		{
+			window.draw(textDisplay[i]);
+		}
 		window.display();
 	}
 	// we need to go over the plugboard settings and potentially change structure of program to make sure that the correct values are being sent to the engima machine code
 	void SetupPlugboard()
 	{
+		std::vector<sf::CircleShape> vector;
+		std::vector<sf::Text> textDisplay;
 		sf::RenderWindow window;
 		window.create(sf::VideoMode({ 800, 600 }), "Plugboard Configuration");
 		sf::Font font;
@@ -131,12 +214,17 @@ public:
 		textDisplayTitle.setPosition({ 350.f,0 });
 		textDisplayTitle.setCharacterSize(24);
 
-		
+		InitializeText(textDisplay, { 65.f,static_cast<float>(window.getSize().y) / 3.3f }, font, InitializeCircles(vector, { 50.f,static_cast<float>(window.getSize().y) / 2.7f }));
 		while (window.isOpen())
 		{
-			RenderValues(window, textDisplayTitle, background);
+			RenderValues(window, textDisplayTitle, background, vector,textDisplay);
 		}
 	}
+
+
+	//rendering the plugboard circles to the screen
+
+
 
 
 
